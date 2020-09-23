d88cdd1f15142e2fa29e86bc948030da71899be9
ArielDavidHerrera <39654239+ArielDavidHerrera@users.noreply.github.com>
2020-09-23 17:25:59 -0500

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ recep }}</div>
+  <button v-on:click="sendMessage(1)">Enviar inicio al servidor</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SocketIO from 'socket.io-client'
+ 
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      connection: null,
+      recep: "algun nombre"
+    };
+  },
+  methods: {
+    sendMessage: function(message) {
+      // conect the socket
+      this.connection = SocketIO('http://localhost:3000/');
+      this.connection.emit('send_emit', message);
+      this.connection.on('emit_message', function(msg){
+        console.log("si esta adentro" + msg);
+        this.recep = msg + " - ";
+        console.log(this.recep);
+      });
+    },
+  },
+  
 }
 </script>
 
